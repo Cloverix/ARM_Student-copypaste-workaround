cc5f685913ee7694061bd8ca0ec6ab40bf385a5f
Cloverix <efremenko160107@mail.ru>
2026-02-14 10:55:11 +0300

    Script status now typed in console

--- CopyMacros/main.py
+++ CopyMacros/main.py
@@ -7,11 +7,13 @@ def paste():
     global stringToType
     sleep(0.1)
     stringToType = clip.paste().replace("\r\n", "\n").replace("public:\n\t", "public:\n    ").replace("private:\n\t", "private:\n    ").replace("protected:\n\t", "protected:\n    ").replace("\t", "")
-    print(repr(stringToType))
+    print("Copied:", repr(stringToType))
 
 def write():
     global stringToType
+    print("Typing...")
     kb.write(stringToType, delay=0.1, restore_state_after=False)
+    print("Type completed")
 
 stringToType = ""
 kb.add_hotkey("ctrl+c", paste)
